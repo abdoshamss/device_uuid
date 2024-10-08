dab347e023a5573f14e551b30b82f0fc8df6a231
abdoshamss <abdoshams2005@gmail.com>
2024-10-08 16:05:49 +0300

    f

---
+++ example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,21 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<device_uuid/DeviceUuidPlugin.h>)
+#import <device_uuid/DeviceUuidPlugin.h>
+#else
+@import device_uuid;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [DeviceUuidPlugin registerWithRegistrar:[registry registrarForPlugin:@"DeviceUuidPlugin"]];
+}
+
+@end
